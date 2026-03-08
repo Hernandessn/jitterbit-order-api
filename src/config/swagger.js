ba6bc3b84c0feff177540c529b7d2d4e5e5cb029
Hernandessn <hernandessalesneves7@gmail.com>
2026-03-08 10:41:16 -0300

    feat: add server entry point and Swagger UI

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,3 +1,5 @@
+const swaggerJsdoc = require('swagger-jsdoc')
+
 const options = {
     definition: {
         openapi: '3.0.0',
--- src/server.js
+++ src/server.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
+dotenv.config();
+
 const connectDB = require('./config/database');
+
+// Swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+
+// Routes
+const orderRoutes = require('./routes/orderRoutes');
+const authRoutes = require('./routes/authRoutes');
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
 connectDB();
+
+// Routes
+app.use('/order', orderRoutes);
+app.use('/auth', authRoutes);
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
