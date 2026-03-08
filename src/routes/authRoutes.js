8cdc3ea83e344017d2fae609a8b8087a18cbfeab
Hernandessn <hernandessalesneves7@gmail.com>
2026-03-08 09:56:03 -0300

    feat: add auth routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const { login } = require('../controllers/authController');
+
+// POST /auth/login - Generate JWT token
+router.post('/login', login);
+
+module.exports = router;
